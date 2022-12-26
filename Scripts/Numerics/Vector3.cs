9970cb34123fcd60782c7a25907be95128296b56
Samuel <70766878+swzwij@users.noreply.github.com>
2022-12-26 17:23:06 +0100

    orderd all scripts into folders

---
+++ Scripts/Numerics/Vector3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeetleEngine
+{
+    public class Vector3
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+        public double z { get; set; }
+
+        public Vector3(double X, double Y, double Z)
+        {
+            x = X;
+            y = Y;
+            z = Z;
+        }
+    }
+}
